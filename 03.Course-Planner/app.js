9b7b859425b66c0a84c7edc57fdf349c786e4077
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-08-17 00:12:00 +0300

    Commit 03.Course-Planner with multiple appends with Fragment.

---
+++ 03.Course-Planner/app.js
@@ -0,0 +1,62 @@
+const tasksUrl = `http://localhost:3030/jsonstore/tasks`;
+
+const loadButtonElement = document.querySelector("#load-course");
+loadButtonElement.addEventListener("click", async () => {
+  const response = await fetch(tasksUrl);
+  const data = await response.json();
+
+  const courses = Object.values(data);
+  const courseListElement = document.querySelector("#list");
+
+  const coursesFragment = document.createDocumentFragment();
+
+  courses
+    .map((course) => renderCourse(course))
+    .forEach((courseElement) => coursesFragment.appendChild(courseElement));
+
+  courseListElement.appendChild(coursesFragment);
+});
+
+function renderCourse(course) {
+  /* 
+    <div class="container">
+        <h2>JS Back-End</h2>
+        <h3>John Brown</h3>
+        <h3>Long</h3>
+        <h4>JS Back-end responsible for managing the interchange of data between the server
+            and the users</h4>
+        <button class="edit-btn">Edit Course</button>
+      <button class="finish-btn">Finish Course</button>
+    </div> 
+*/
+  const headingElement = document.createElement("h2");
+  headingElement.textContent = course.title;
+
+  const teacherElement = document.createElement("h3");
+  teacherElement.textContent = course.teacher;
+
+  const typeElement = document.createElement("h3");
+  typeElement.textContent = course.type;
+
+  const descriptionElement = document.createElement("h4");
+  descriptionElement.textContent = course.description;
+
+  const editButtonElement = document.createElement("button");
+  editButtonElement.className = "edit-btn";
+  editButtonElement.textContent = "Edit Course";
+
+  const finishButtonElement = document.createElement("button");
+  finishButtonElement.className = "finish-btn";
+  finishButtonElement.textContent = "Finish Course";
+
+  const courseContainer = document.createElement("div");
+  courseContainer.className = "container";
+  courseContainer.appendChild(headingElement);
+  courseContainer.appendChild(teacherElement);
+  courseContainer.appendChild(typeElement);
+  courseContainer.appendChild(descriptionElement);
+  courseContainer.appendChild(editButtonElement);
+  courseContainer.appendChild(finishButtonElement);
+
+  return courseContainer;
+}
